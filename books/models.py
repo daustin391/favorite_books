d5fae76fc510f72aea957af7520ab057d258f3dd
Dave Austin <d.austin_391@yahoo.com>
2020-11-14 09:58:16 -0500

    book page can add entry to db

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from login.models import User
 
-# Create your models here.
+
+class BookManager(models.Manager):
+    def add_book(self, form_data, user_id):
+        Book.objects.create(
+            title=form_data["title"],
+            desc=form_data["desc"],
+            uploaded_by=User.objects.get(id=user_id),
+        )
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    uploaded_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="books_uploaded"
+    )
+    objects = BookManager()
--- books/views.py
+++ books/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from login.models import User
+from .models import Book
 
 
 def index(request):
     context = {"this_user": User.objects.get(id=request.session["id"])}
     return render(request, "books.html", context)
+
+
+def add_book(request):
+    if request.method == "POST":
+        Book.objects.add_book(request.POST, request.session["id"])
+    return redirect("/books")
